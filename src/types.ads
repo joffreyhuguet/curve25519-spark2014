6894bc07a83c2366d6425c674312fdbd5eb4ca69
Joffrey Huguet <huguet.joffrey@gmail.com>
2019-02-25 15:33:26 +0100

    Error in types.ads fixed

--- src/types.ads
+++ src/types.ads
@@ -4,11 +4,11 @@ package Types with
   SPARK_Mode
 is
 
-   type Extended_Index_Type is new Integer range - 1 .. 9;
+   type Extended_Index_Type is new Integer range - 1 .. 18;
    subtype Index_Type_Mult is Extended_Index_Type range 0 .. 18;
    subtype Index_Type is Extended_Index_Type range 0 .. 9;
 
-   type Ints is array (Index_Type range <>) of Long_Long_Integer;
+   type Ints is array (Index_Type_Mult range <>) of Long_Long_Integer;
    subtype Ints_Mult is Ints (0 .. 18);
    subtype Ints_256 is Ints (0 .. 9) with
      Dynamic_Predicate => (for all J of Ints_256 => J in -2**31 .. 2**31 - 1);
